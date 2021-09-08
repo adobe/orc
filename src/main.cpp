8897754cf9ac8430646a3fa92403f3936e4a8783
Lee Thomason <lthomaso@adobe.com>
2021-09-08 16:43:47 -0700

    move static inside block

--- src/main.cpp
+++ src/main.cpp
@@ -136,8 +136,8 @@ void report_odrv(const std::string_view& symbol, const die& a, const die& b, dw:
     // A, B, then C -> 1 ODRV, but C, then A, B -> 2 ODRVs. Complicating
     // this is that (as this comment is written) we don't detect supersets.
     // So if C has more methods than A and B it may not be detected.
-    static std::set<std::size_t> unique_odrv_types;
     if (settings._filter_redundant) {
+        static std::set<std::size_t> unique_odrv_types;
         std::size_t symbol_hash = hash_combine(0, symbol, odrv_category);
         bool did_insert = unique_odrv_types.insert(symbol_hash).second;
         if (!did_insert) return; // We have already reported an instance of this.

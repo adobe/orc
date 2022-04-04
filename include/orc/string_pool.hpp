e2a037b8fb9fc25055402c2c70498c0d7180f6d7
Lee Thomason <lthomaso@adobe.com>
2022-04-04 13:32:46 -0700

    comments, testing, per-thread pools

--- include/orc/string_pool.hpp
+++ include/orc/string_pool.hpp
@@ -16,17 +16,47 @@
 
 struct pool_string;
 
+/*
+    Add a string to the pool. This is not thread safe.
+
+    A previous implementatino of empool() used a mutex (as a test) which reduced ORC's memoory
+    consumption from 83GB to 47GB. That's pretty good, but there is work (and complexity) to
+    making the string pool thread safe.
+
+    Surprisingly, using a string pool *per thread* reduces the memory usage from 83GB to 53GB,
+    for no noticeable performance impact. A result I find somewhat counter-intuitive. Theory 
+    as to why the simple per thread solution works as well as it does:
+
+    * the highly used `struct die` is much smaller. (`pool_string` is now one pointer instead of 3, and a `std::string` became a `pool_string`)
+    * the threads have good coherency; a given thread is generally processing a compilation unit, and the same strings repeat
+    * and (in both solutions) the memory to store strings is greatly reduced, since they are stored in un-aligned blocks of packed memory
+*/
 pool_string empool(std::string_view src);
 
+/*
+    A pool_string is an interned string. Once created, the pointer `_data` is immutable for the
+    life of the application. This has some useful properties.
+
+    * A `pool_string` is one pointer in size
+    * A `pool_string` is thread safe
+    * Two `pool_strings` pointing to the same `_data` are always equal.
+    * Becaues `pool_strings` can be in different pools, if the `_data` members are different, they may still be equal
+    * `_data` is a char* to null terminated data, which is just a c string. Useful for debugging. 
+    * if `_data` is null, it is intepreted as an empty string (""). If `_data` is not-null, it always is size() > 0
+    
+    When empooled, the hash (64 bits) and size/length (32 bits) are stored before the char* to the data.
+    Note there is no memory alignment in the pool - it is fully packed - so data needs to be memcpy'd in
+    and out, just in case the processor doesn't like un-aligned reads.
+*/
 struct pool_string {
     pool_string() {}
     ~pool_string() {}
 
-    auto empty() const { return _data == nullptr; }
+    bool empty() const { return _data == nullptr; }
 
     std::string_view view() const {
         if (!_data) return default_view;
-        std::uint32_t size = get_length(_data);
+        std::uint32_t size = get_size(_data);
         return std::string_view(_data, size);
     }
     
@@ -45,7 +75,9 @@ struct pool_string {
     }
 
     friend inline bool operator==(const pool_string& x, const pool_string& y) {
-        return x._data == y._data;
+        if (x._data == y._data)
+            return true;
+        return x.view() == y.view();
     }
 
     friend inline bool operator!=(const pool_string& x, const pool_string& y) {
@@ -57,17 +89,16 @@ struct pool_string {
     }
 
 private:
-    static std::uint32_t get_length(const char* d) {
-        assert(d > (const char*)1024);
+    static std::size_t get_size(const char* d) {
         const void* bytes = d - sizeof(std::uint32_t) - sizeof(std::size_t);
         std::uint32_t s;
         std::memcpy(&s, bytes, sizeof(s));
-        assert(s > 0 && s < 100000);
+        assert(s > 0);         // required, else should have been _data == nullptr
+        assert(s < 100000);    // sanity check
         return s;
     }
 
     static std::size_t get_hash(const char* d) {
-        assert(d > (const char*)1024);
         const void* bytes = d - sizeof(std::size_t);
         std::size_t h;
         std::memcpy(&h, bytes, sizeof(h));
@@ -75,11 +106,8 @@ private:
     }
 
     friend pool_string empool(std::string_view src);
-    static std::string_view default_view;
+    static std::string_view default_view; // an empty string
 
-    //explicit pool_string(std::string_view s, std::size_t h) : _data( {}
-    //explicit pool_string(std::string_view s) : _s{std::move(s)},
-    //_h{std::hash<std::string_view>{}(_s)} {}
     explicit pool_string(const char* data) : _data(data) {
     }
     
@@ -87,12 +115,8 @@ private:
     // Before the _data pointer:
     //      'uint32_t' length of string
     //      'size_t' hash
-    // _data[]
-
+    // _data[] null terminated to ease debugging
     const char* _data{nullptr};
-
-    //std::string_view _s;
-    //std::size_t _h{0};
 };
 
 /**************************************************************************************************/
--- src/string_pool.cpp
+++ src/string_pool.cpp
@@ -30,7 +30,7 @@ namespace {
 
 
 struct pool {
-    char* _p;
+    char* _p{nullptr};
     std::size_t _n{0};
     std::vector<std::unique_ptr<char[]>> _ponds;
     std::size_t _total_alloc = 0;
@@ -43,7 +43,6 @@ struct pool {
         if (_n < tsz) {
             _n = std::max<std::size_t>(default_min_k, tsz);
             _total_alloc += _n;
-            printf("Total string pool mem: %zu\n", _total_alloc);
             _ponds.push_back(std::make_unique<char[]>(_n));
             _p = _ponds.back().get();
         }
@@ -60,51 +59,13 @@ struct pool {
     }
 };
  
-/**************************************************************************************************/
-
-/*
-auto& pool() {
-#if ORC_FEATURE(LEAKY_MEMORY)
-    thread_local struct pool& instance = *(new struct pool());
-#else
-    thread_local struct pool instance;
-#endif // ORC_FEATURE(LEAKY_MEMORY)
-
-    return instance;
-}
- */
 
 /**************************************************************************************************/
 
-struct pool_string_hash {
-    auto operator()(const pool_string& x) const {
-        return x.hash();
-    }
-};
-
-struct pool_string_equal_to {
-    auto operator()(const pool_string& x, const pool_string& y) const {
-        return x == y;
-    }
-};
-
+// The key is the hash; map one to the other.
 struct pool_key_to_hash {
     auto operator()(size_t key) const { return key;}
 };
-
-/*
-auto& set() {
-    using set_type = std::unordered_set<pool_string, pool_string_hash, pool_string_equal_to>;
-
-#if ORC_FEATURE(LEAKY_MEMORY)
-    thread_local set_type& instance = *(new set_type());
-#else
-    thread_local set_type instance;
-#endif
-
-    return instance;
-}
-*/
 
 /**************************************************************************************************/
 
@@ -116,13 +77,8 @@ pool_string empool(std::string_view src) {
     if (src.empty())
         return pool_string(nullptr);
     
-    static std::mutex mutex;
-    std::lock_guard<std::mutex> guard(mutex);
-    
-    //using set_type = std::unordered_set<pool_string, pool_string_hash, pool_string_equal_to>;
-    //static set_type s;
-    static std::unordered_multimap<size_t, const char*, pool_key_to_hash> keys;
-    static pool the_pool;
+    thread_local std::unordered_multimap<size_t, const char*, pool_key_to_hash> keys;
+    thread_local pool the_pool;
     
     // Is the string interned already?
     const size_t h = std::hash<std::string_view>{}(src);
@@ -131,34 +87,14 @@ pool_string empool(std::string_view src) {
     for(auto it = range.first; it != range.second; ++it) {
         pool_string ps(it->second);
         if (ps.view() == src) {
-            return ps; // check the string_view is equal! not the pool pointer.
+            return ps;
         }
     }
     
+    // Not already interned; empool it and add to the 'keys' 
     const char* ptr = the_pool.empool(src);
-    assert(*ptr >= 32);
     keys.insert({{h, ptr}});
-    if (memcmp(ptr, "XML_Size\\", 9) == 0)
-        printf("d");
     return pool_string(ptr);
-    
-    /*
-    
-       
-    pool_string needle(src);
-    auto found = s.find(needle);
-
-    if (found != s.end()) {
-        return *found;
-    }
-
-    pool_string empooled(pool().empool(src), needle.hash());
-    assert(std::hash<std::string_view>{}(empooled._s) == empooled._h);
-
-    auto result = s.insert(std::move(empooled));
-    assert(result.second);
-
-    return pool_string(*result.first);*/
 }
 
 /**************************************************************************************************/

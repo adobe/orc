1d370e7904d3ac3b1cd59ddbd573f4e917bc39a1
Foster Brereton <fbrereto@adobe.com>
2022-05-17 10:28:03 -0700

    Updating YAML

--- .github/generate_job_summary.py
+++ .github/generate_job_summary.py
@@ -9,16 +9,16 @@ if __name__ == "__main__":
     sys.stdout = open(sys.argv[1], "w")
 
     github = json.load(sys.argv[2])
-    job = json.load(sys.argv[3])
-    steps = json.load(sys.argv[4])
-    runner = json.load(sys.argv[5])
-    strategy = json.load(sys.argv[6])
-    matrix = json.load(sys.argv[7])
+    # job = json.load(sys.argv[3])
+    # steps = json.load(sys.argv[4])
+    # runner = json.load(sys.argv[5])
+    # strategy = json.load(sys.argv[6])
+    # matrix = json.load(sys.argv[7])
 
     print("# Job Summary")
     print("| Run | Result | Notes |")
     print("|---|---|---|")
 
-    for key in steps:
-        value = steps[key];
-        print(f"| {key} | {value.outcome} | :rocket: |")
+    # for key in steps:
+    #     value = steps[key];
+    #     print(f"| {key} | {value.outcome} | :rocket: |")

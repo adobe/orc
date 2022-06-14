61546d0baa4a4755720ac5b8fea88c7e1e24a4f9
Foster Brereton <fbrereto@adobe.com>
2022-06-14 16:03:40 -0700

    wip

--- .github/orc_test_to_github_actions.py
+++ .github/orc_test_to_github_actions.py
@@ -4,4 +4,4 @@ import json
 if __name__ == "__main__":
     step_name = sys.argv[1];
     test_results = json.load(open(sys.argv[2], "r"))
-    print(f"::set-output step_name::{test_results}");
+    print(f"::set-output name={step_name}::{test_results}");

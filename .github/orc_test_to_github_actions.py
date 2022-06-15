fdf89ce5cac3199940d1f1ca0feab98a6b9502a9
Foster Brereton <fbrereto@adobe.com>
2022-06-14 17:02:42 -0700

    wip

--- .github/orc_test_to_github_actions.py
+++ .github/orc_test_to_github_actions.py
@@ -2,5 +2,5 @@ import sys
 import json
 
 if __name__ == "__main__":
-    test_results = json.load(open(sys.argv[2], "r"))
+    test_results = json.load(open(sys.argv[1], "r"))
     print(f"::set-output name=orc_test_out::{test_results}");

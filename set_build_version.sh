9b8f8841168a2f76b38f9ca5c206c4bab39762f1
Foster Brereton <fbrereto@adobe.com>
2024-05-29 10:07:14 -0700

    updating version script

--- set_build_version.sh
+++ set_build_version.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "hello, world!"
+cd $(dirname "$0")
+
+VERSION_HEADER=./include/orc/version.hpp
+VERSION_STR="$1"
+SHA_STR="$2"
+
+echo "#define ORC_VERSION_STR() \"$VERSION_STR\"" > $VERSION_HEADER
+echo "#define ORC_SHA_STR() \"$SHA_STR\"" >> $VERSION_HEADER
+
+echo "Version: $VERSION_STR"
+echo "SHA: $SHA_STR"
